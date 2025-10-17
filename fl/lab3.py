78fc24d43e653d2ef6fff2e33edea6d6448f11eb
kiril <kirillkravcenko029@gmail.com>
2025-10-17 10:13:26 +0300

    Задание 2

--- fl/lab3.py
+++ fl/lab3.py
@@ -1,2 +1,5 @@
-squares = [x**2 for x in range(1, 11)]
-print(squares)
+# squares = [x**2 for x in range(1, 11)]
+# print(squares)
+
+even_numbers = [x for x in range(2, 20) if x % 2 == 0]
+print(even_numbers)
