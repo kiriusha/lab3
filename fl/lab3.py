3d6cabea40cf4e967a6bbc155329d8249f11af6a
kiril <kirillkravcenko029@gmail.com>
2025-10-17 10:24:10 +0300

    Задание 4

--- fl/lab3.py
+++ fl/lab3.py
@@ -4,6 +4,24 @@
 # even_numbers = [x for x in range(2, 20) if x % 2 == 0]
 # print(even_numbers)
 
-words = ["python", "Java", "c++", "Rust", "go"]
-upper_words = [word.upper() for word in words if len(word) >= 3]
-print(upper_words)
+# words = ["python", "Java", "c++", "Rust", "go"]
+# upper_words = [word.upper() for word in words if len(word) >= 3]
+# print(upper_words)
+
+class Countdown:
+    def __init__(self, n):
+        self.n = n
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n < 1:
+            raise StopIteration
+        current = self.n
+        self.n -= 1
+        return current
+
+
+for x in Countdown(5):
+    print(x)
