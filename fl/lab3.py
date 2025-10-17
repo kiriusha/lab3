750157e3aa185ae699a1b32777defc8e6703d637
kiril <kirillkravcenko029@gmail.com>
2025-10-17 10:20:56 +0300

    Задание 3

--- fl/lab3.py
+++ fl/lab3.py
@@ -1,5 +1,9 @@
 # squares = [x**2 for x in range(1, 11)]
 # print(squares)
 
-even_numbers = [x for x in range(2, 20) if x % 2 == 0]
-print(even_numbers)
+# even_numbers = [x for x in range(2, 20) if x % 2 == 0]
+# print(even_numbers)
+
+words = ["python", "Java", "c++", "Rust", "go"]
+upper_words = [word.upper() for word in words if len(word) >= 3]
+print(upper_words)
